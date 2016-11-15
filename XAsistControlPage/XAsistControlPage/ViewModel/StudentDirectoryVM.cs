1d340992432eedb11fbba54b411519b799fe2ba4
David Zambrano <davazamb10@outlook.com>
2016-11-15 11:42:17 -0400

    Creando una clase viewmodel

---
+++ XAsistControlPage/XAsistControlPage/ViewModel/StudentDirectoryVM.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using XAsistControlPage.Model.Entities;
+using XAsistControlPage.Model.Services;
+
+namespace XAsistControlPage.ViewModel
+{
+    public class StudentDirectoryVM
+    {
+        //Coleccion para mostrar el listado de estudiantes
+        public ObservableCollection<Student> Students { get; set; }
+
+        bool isBusy = false;
+        public bool IsBusy
+        {
+            get { return isBusy; }
+            set { isBusy = value; }
+        }
+        //LLamados Command
+        public Command LoadDirectoryCommand { get; set; }
+
+        //Constructor
+        public StudentDirectoryVM ()
+        {
+            Students = new ObservableCollection<Student>();
+            IsBusy = false;
+            LoadDirectoryCommand = new Command((obj) => LoadDirectory());
+        }
+
+        private async void LoadDirectory()
+        {
+            if(!IsBusy)
+            {
+                IsBusy = true;
+                await Task.Delay(3000);
+                var loadedDirectory = StudentDirectoryService.LoadStudentDirectory();
+
+                foreach (var student in loadedDirectory.Students)
+                {
+                    Students.Add(student);
+                }
+                IsBusy = false;
+            }
+        }
+    }
+}
