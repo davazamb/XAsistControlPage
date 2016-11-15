1745e7005acab15e9d1917b31b055a8baa76f863
David Zambrano <davazamb10@outlook.com>
2016-11-15 11:27:12 -0400

    Creacion de modelos student y student directory service y viewmodels

---
+++ XAsistControlPage/XAsistControlPage/Model/Entities/Student.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XAsistControlPage.Model.Entities
+{
+    public class Student
+    {
+
+
+        private string name;
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        private string lastName;
+
+        public string LastName
+        {
+            get { return lastName; }
+            set { lastName = value; }
+        }
+
+        private string group;
+
+        public string Group
+        {
+            get { return group; }
+            set { group = value; }
+        }
+
+        private string studentNumber;
+
+        public string StudentNumber
+        {
+            get { return studentNumber; }
+            set { studentNumber = value; }
+        }
+
+        private double average;
+
+        public double Average
+        {
+            get { return average; }
+            set { average = value; }
+        }
+
+    }
+}
